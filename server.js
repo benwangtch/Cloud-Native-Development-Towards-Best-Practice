a6842b96627bab1d03ffea09b82ae3bf404046a0
benwang <ben2820@gmail.com>
2023-05-31 23:14:55 +0800

    first

--- server.js
+++ server.js
@@ -32,5 +32,5 @@ if (process.env.NODE_ENV === 'default') {
   );
 }
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
